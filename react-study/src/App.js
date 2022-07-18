8ba2d101d4dbbde5e950a8502d0c94a6b0d88ffd
hyelin <k1h2fls@gmail.com>
2022-07-18 15:18:49 +0900

    ch8.2 useEffect

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
-import Counter from "./ch8-Counter";
+import React, { useState } from "react";
+import Info from "./practice/ch8-info";
 
 const App = () => {
-  return <Counter />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 };
 
 export default App;
